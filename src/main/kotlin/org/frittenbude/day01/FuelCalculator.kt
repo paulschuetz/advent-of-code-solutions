2eae2e1ce787fdba450c7595850135545de6320b
pschuetz <paulschuetz.mail@gmail.com>
2019-12-01 15:22:45 +0100

    solved second quiz of day 1

--- src/main/kotlin/org/frittenbude/day01/FuelCalculator.kt
+++ src/main/kotlin/org/frittenbude/day01/FuelCalculator.kt
@@ -1,7 +1,22 @@
 package org.frittenbude.day01
 
 class FuelCalculator{
-    fun calculateFuel(components: List<Int>) : Int {
-        return components.stream().mapToInt{ (it / 3) - 2 }.sum()
+
+    private fun calculateFuelForComponent(componentMass: Int) : Int {
+        return (componentMass / 3) - 2
+    }
+
+    fun calculateFuelForComponents(components: List<Int>) : Int {
+        return components.stream().mapToInt{ calculateFuelForComponent(it) }.sum()
+    }
+
+    fun calculateTotalFuelForComponents(components: List<Int>) : Int{
+        return components.stream().mapToInt{ calculateTotalFuelForComponent(it) }.sum()
+    }
+
+    fun calculateTotalFuelForComponent(component: Int) : Int {
+        val fuel = calculateFuelForComponent(component)
+        return if(fuel < 0) 0
+        else fuel + calculateTotalFuelForComponent(fuel)
     }
 }
--- src/test/kotlin/org/frittenbude/day01/FuelCalculatorTest.kt
+++ src/test/kotlin/org/frittenbude/day01/FuelCalculatorTest.kt
@@ -7,15 +7,29 @@ import java.util.stream.Collectors
 
 
 class FuelCalculatorTest{
+
     @Test
-    fun testCalculateFuel(){
-        val fuel = FuelCalculator().calculateFuel(arrayListOf(10, 15, 8 ))
-        Assert.assertSame(4, fuel)
+    fun printSolution01(){
+        val components = File("src/test/resources/input01.txt").readLines().toList().stream().map { Integer.parseInt(it) }.collect(Collectors.toList())
+        println(FuelCalculator().calculateFuelForComponents(components))
     }
 
     @Test
-    fun printSolution(){
+    fun printSolution02(){
         val components = File("src/test/resources/input01.txt").readLines().toList().stream().map { Integer.parseInt(it) }.collect(Collectors.toList())
-        println(FuelCalculator().calculateFuel(components))
+        println(FuelCalculator().calculateTotalFuelForComponents(components))
+    }
+
+    @Test
+    fun testCalculateFuel(){
+        val fuel = FuelCalculator().calculateFuelForComponents(arrayListOf(10, 15, 8 ))
+        Assert.assertSame(4, fuel)
+    }
+
+    // I literally have no clue while this is failing although the results are identical
+    @Test
+    fun testCalculateTotalFuelForComponent(){
+        val totalFuel = FuelCalculator().calculateTotalFuelForComponent(1969)
+        Assert.assertSame("TotalFuelForComponent", 966, totalFuel)
     }
 }
